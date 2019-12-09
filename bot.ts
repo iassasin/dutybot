56b7abf032d08d98f0cf43c94c7f7d9b6c99848f
iassasin <iassasin@yandex.ru>
2019-12-09 21:20:50 +0300

    add group to subscribes

--- bot.ts
+++ bot.ts
@@ -4,15 +4,33 @@ import * as KoaRouter from '@koa/router';
 import * as YAML from 'yaml';
 import * as fs from 'fs';
 
+import Storage, { BotStorage } from './src/storage';
+
 const config = YAML.parse(fs.readFileSync(`${__dirname}/../config.yml`).toString());
+
+let storage = new Storage<BotStorage>(`${__dirname}/../data.json`, {
+	dutyMans: [],
+	dutySlots: [],
+	subscribedChats: [],
+});
 
 let bot = new Telegraf(config.bot.token);
 bot.command('/subscribe_group', async ctx => {
 	let {message} = ctx.update;
+
 	if (message.from.id === config.bot.ownerId) {
-		ctx.reply('Subscribed!');
+		if (!storage.data.subscribedChats.some(x => x.id === ctx.chat.id)) {
+			storage.mutate(data => data.subscribedChats.push({
+				id: ctx.chat.id,
+				title: ctx.chat.title,
+				type: ctx.chat.type,
+			}));
+			ctx.reply('Группа подписана!');
+		} else {
+			ctx.reply('Группа уже подписана!');
+		}
 	} else {
-		ctx.reply('No permission to rule bot!');
+		ctx.reply('Недостаточно прав!');
 	}
 });
 bot.launch();
---
+++ src/storage.ts
@@ -0,0 +1,52 @@
+import * as fs  from 'fs';
+
+export interface Chat {
+	id: number;
+	title: string;
+	type: string;
+}
+
+export interface DutyMan {
+	id: number;
+	username: string;
+}
+
+export interface DutySlot {
+	userId: number;
+	startTime: string;
+}
+
+export interface BotStorage {
+	subscribedChats: Chat[];
+	dutyMans: DutyMan[];
+	dutySlots: DutySlot[];
+}
+
+export default class Storage<T> {
+	private dataObj: T = {} as T;
+	readonly filename: string;
+
+	get data() { return this.dataObj; }
+
+	constructor(filename: string, defaultObj: T = {} as T) {
+		this.filename = filename;
+		this.dataObj = defaultObj;
+	}
+
+	reload() {
+		try {
+			this.dataObj = JSON.parse(fs.readFileSync(this.filename).toString());
+		} catch (e) {
+			console.error(`Can't read data from file '${this.filename}'`, e);
+		}
+	}
+
+	mutate(mutator: (data: T) => void) {
+		mutator(this.dataObj);
+		try {
+			fs.writeFileSync(this.filename, JSON.stringify(this.dataObj));
+		} catch (e) {
+			console.error(`Can't write data to file '${this.filename}'`, e);
+		}
+	}
+}
